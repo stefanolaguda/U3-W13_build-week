d867eb87c36756facb59ec070e07f82dfe97d664
EarthDragon10 <lagudadev@gmail.com>
2022-11-14 12:33:33 +0100

    add register html

--- my-crm/src/app/auth/register/register.component.ts
+++ my-crm/src/app/auth/register/register.component.ts
@@ -1,15 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ViewChild } from '@angular/core';
-import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
-import { Observable, Subject, merge, OperatorFunction } from 'rxjs';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  filter,
-  map,
-} from 'rxjs/operators';
-
-const states = ['user', 'admin'];
 
 @Component({
   selector: 'app-register',
@@ -17,37 +6,8 @@ const states = ['user', 'admin'];
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  model: any;
-
-  @ViewChild('instance', { static: true }) instance: NgbTypeahead | undefined;
-  focus$ = new Subject<string>();
-  click$ = new Subject<string>();
-
+  roles = ['admin', 'user'];
   constructor() {}
-
-  search: OperatorFunction<string, readonly string[]> = (
-    text$: Observable<string>
-  ) => {
-    const debouncedText$ = text$.pipe(
-      debounceTime(200),
-      distinctUntilChanged()
-    );
-    const clicksWithClosedPopup$ = this.click$.pipe(
-      filter(() => !this.instance!.isPopupOpen())
-    );
-    const inputFocus$ = this.focus$;
-
-    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
-      map((term) =>
-        (term === ''
-          ? states
-          : states.filter(
-              (v) => v.toLowerCase().indexOf(term.toLowerCase()) > -1
-            )
-        ).slice(0, 10)
-      )
-    );
-  };
 
   ngOnInit(): void {}
 }
