a807fad4b9d2b7ebed807b1f3d9c351216113bbf
EarthDragon10 <lagudadev@gmail.com>
2022-11-14 11:09:29 +0100

    added header and footer

--- my-crm/src/app/app-routing.module.ts
+++ my-crm/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePagePage } from './pages/home-page/home-page.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePagePage,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
