ce12845a7336e6db971e4b343724ab2fc4ac5b31
EarthDragon10 <lagudadev@gmail.com>
2022-11-16 16:01:29 +0100

    s

--- my-crm/src/app/pages/client-list/client-list.page.ts
+++ my-crm/src/app/pages/client-list/client-list.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Client } from 'src/app/classes/client';
 import { ClientService } from 'src/app/services/client.service';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms';
+import { NgModelGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   templateUrl: './client-list.page.html',
@@ -12,13 +14,27 @@ export class ClientListPage implements OnInit {
 
   clientList?: Client[];
   currentClient?: Client;
+  addressGroup!: FormGroup;
+  comuneFroup!: FormGroup;
 
-  constructor(private clientServ: ClientService) {}
+  constructor(private clientServ: ClientService, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.clientServ.getClient().subscribe((data) => {
       this.clientList = data;
     });
+
+    this.addressGroup = new FormGroup({
+      via: new FormControl(),
+      age: new FormControl(),
+      cap: new FormControl(),
+    });
+
+    // this.addressGroup = this.fb.group(
+    //   via: [],
+    //   civico: [],
+    //   cap: []
+    // )
   }
 
   onSubmit() {
