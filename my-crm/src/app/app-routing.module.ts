58caf73cb64c056575f49ce5449fa11e15406af3
EarthDragon10 <lagudadev@gmail.com>
2022-11-17 12:35:05 +0100

    fix routing of login and register

--- my-crm/src/app/app-routing.module.ts
+++ my-crm/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { ClientListPage } from './pages/client-list/client-list.page';
 import { HomePagePage } from './pages/home-page/home-page.page';
 
@@ -10,7 +12,15 @@ const routes: Routes = [
   },
   {
     path: 'clients',
-    component: ClientListPage
+    component: ClientListPage,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
   {
     path: '**',
